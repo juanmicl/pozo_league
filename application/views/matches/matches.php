b03968873666488683e0bfae9a02d274c80a9ab8
juanmicl <juanmicontlup@gmail.com>
2020-03-24 13:10:42 +0100

    matches responsive

--- application/views/matches/matches.php
+++ application/views/matches/matches.php
@@ -1,7 +1,7 @@
 <div class="container mt-3">
     <div class="row">
         <?php foreach($matches as $match) { ?>
-        <div class="col-6 mb-3">
+        <div class="col-md-6 mb-3">
             <div class="card border-0 shadow-sm">
                 <div class="card-header">
                     <div class="row">
@@ -18,7 +18,7 @@
                 </div>
                 <div class="card-body text-center">
                     <div class="row">
-                        <div class="col-5">
+                        <div class="col-sx-5">
                             <h5 class="text-success">Victoria</h5>
                             <?php foreach ($match['players'] as $player) { 
                             if ($player['win'] == 1) {    
@@ -31,7 +31,7 @@
                             </div>
                             <?php } } ?>
                         </div>
-                        <div class="col-2">
+                        <div class="col-sx-2">
                             <h5>VS</h5>
                             <img src="/assets/img/positions/top.png" style="width: 25px;height: 25px;"><br>
                             <img src="/assets/img/positions/mid.png" style="width: 25px;height: 25px;"><br>
@@ -39,7 +39,7 @@
                             <img src="/assets/img/positions/bot.png" style="width: 25px;height: 25px;"><br>
                             <img src="/assets/img/positions/bot.png" style="width: 25px;height: 25px;">
                         </div>
-                        <div class="col-5">
+                        <div class="col-sx-5">
                             <h5 class="text-danger">Derrota</h5>
                             <?php foreach ($match['players'] as $player) { 
                             if ($player['win'] == 0) {    
